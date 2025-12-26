8afd3f95ca655f0247d02908509c4ccc2c0551e4
Pium <pium@local.com>
2025-12-26 22:33:42 +0900

    25.12.26 - 여러명 접속 기능 추가

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    // 같은 와이파이에서 접속 허용 (192.168.x.x 전체)
+    allowedDevOrigins: ['http://192.168.*', 'http://localhost'],
+
     // 이미지 최적화 설정
     images: {
         remotePatterns: [
